7106035b77cd52a096d06db35ef29148d1893b06
MxInput <mxinput@protonmail.com>
2026-08-30 16:46:10 -0400

    printing out evenly sized board

--- program.c
+++ program.c
@@ -9,14 +9,19 @@
 bool is_playing = true;
 
 void createBoard(char board[ROW_SIZE][COL_SIZE]) {
-    for (int row = 0; row < ROW_SIZE; row++) {
+    for (int row = 0; row < ROW_SIZE + 1; row++) {
         for (int col = 0; col < COL_SIZE; col++) {
-            if (board[row][col] == COM_MOVE || board[row][col] == USER_MOVE)
-                printf(board[row][col]);
-            if (col == 0)
-                printf("| ");
-            else 
-                printf(" |");
+            int actual_row = row - 1;
+
+            printf("| ");
+            if (row == 0)
+                printf("%i ", col);
+            else
+                if (board[actual_row][col] == COM_MOVE || board[actual_row][col] == USER_MOVE)
+                    printf("%c ", board[actual_row][col]);
+                else
+                    printf("  ");
+            printf("|");
         }
         printf("\n");
     }
@@ -55,18 +60,51 @@ void instruct() {
     printf("Enter the column you'd like to be in.\n\n");
 }
 
+void pickCol(char board[ROW_SIZE][COL_SIZE]) {
+    bool invalid_column = true;
+    int picked_col;
+
+    while (invalid_column)
+    {
+        scanf(" %i", &picked_col);
+
+        for (int row = 0; row < ROW_SIZE; row++) {
+            if (board[row][picked_col] != COM_MOVE && board[row][picked_col] != USER_MOVE) {
+                invalid_column = false;
+                break;
+            }
+        }
+
+        if (!invalid_column)
+            break;
+
+        printf("This column is filled! Pick another!");
+    }
+    
+}
+
+void resetBoard(char board[ROW_SIZE][COL_SIZE]) {
+    for (int row = 0; row < ROW_SIZE; row++) {
+        for (int col = 0; col < COL_SIZE; col++) { 
+            board[row][col] = ' ';
+        }
+    }
+}
+
 void playGame() {
     char board[ROW_SIZE][COL_SIZE];
 
     while (is_playing) {
+        resetBoard(board);
         start();
 
         if (!is_playing)
             break;
         
-        createBoard();
+        createBoard(board);
 
         instruct();
+        pickCol(board);
     }
 }
 
